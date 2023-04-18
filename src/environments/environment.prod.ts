4635ec2501421e3bb5f43eb4c9461203cf4f1104
santosh <santhosh.t@nuacem.com>
2023-04-18 10:01:04 +0530

    Auto-generated commit [ci skip]

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  firebase_url: 'https://e-store-23dd9.firebaseio.com',
-  firebase_key: 'AIzaSyBYCyK5h9tFRHN3M2gSnSc6nYLPtdOYkC0',
-  firebase_auth: 'https://identitytoolkit.googleapis.com/v1/',
-};
